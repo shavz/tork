b326fe0a01fe1c6e34269187d76962a4a6d37482
shiveenpandita <shiveenpandita@gmail.com>
2019-03-21 22:25:20 +1100

    dang son

--- src/io/shiveenp/TorrentReader.kt
+++ src/io/shiveenp/TorrentReader.kt
@@ -2,28 +2,40 @@ package io.shiveenp
 
 import java.io.EOFException
 import java.io.File
-import java.io.InputStream
 import java.io.PushbackInputStream
 
 private const val INPUT_STREAM_LOOK_AHEAD = 1
 
 fun readBencodedFile(file: File) {
     val dataBytes = PushbackInputStream(file.inputStream(), INPUT_STREAM_LOOK_AHEAD)
-}
+    dataBytes.use {
 
-fun readNextByte(input: PushbackInputStream) {
-    when(val data = dataBytes.lookAhead().toString()) {
-        "i "-> readInteger()
-        "l" -> readList()
-        "d" -> readDictionary()
     }
 }
 
-fun readDictionary() {
+fun readNextByte(input: PushbackInputStream) {
+    when (val data = input.lookAhead().toString()) {
+        "i " -> readInteger(input)
+        "l" -> readList(input)
+        "d" -> readDictionary(input)
+        else -> readString(input)
+    }
+}
+
+fun readInteger(input:) {
+    input.reader().read()
+}
+
+
+fun readString(input: PushbackInputStream) {
+
+}
+
+fun readDictionary(input: PushbackInputStream) {
     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 }
 
-fun readList() {
+fun readList(input: PushbackInputStream) {
     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 }
 
@@ -42,9 +54,4 @@ fun PushbackInputStream.safeRead(): Byte {
     if (byte == -1) throw EOFException()
     return byte.toByte()
 }
-
-fun readInteger(input: ) {
-    input.reader().read().
-}
-
 
