95eee6539635e495fef842ee218803387e8853ab
shiveenpandita <shiveenpandita@gmail.com>
2019-04-04 22:33:28 +1100

    Add string reader implementation

--- src/io/shiveenp/TorrentReader.kt
+++ src/io/shiveenp/TorrentReader.kt
@@ -2,7 +2,6 @@ package io.shiveenp
 
 import java.io.EOFException
 import java.io.File
-import java.io.InputStream
 import java.io.PushbackInputStream
 
 private const val INPUT_STREAM_LOOK_AHEAD = 1
@@ -26,22 +25,23 @@ fun readNextByte(input: PushbackInputStream) {
 /**
  * Reads an integer type value `i3e` or `i-25e` from an bencoded stream
  */
+// ToDo: needs to differentiate between i03e which is invalid and i0e which is valid
 @Throws(BencodeReaderException::class)
 fun readInteger(input: PushbackInputStream): Int {
     var digitRead = false
     var isNegative = false
     var stillReading = true
 
-    val integerString = ""
+    var integerString = ""
     var integerToReturn = 0
 
     try {
         while (stillReading) {
-            val tmp = input.lookAhead().toString()
+            val tmp = input.safeRead().toString()
             if (tmp == "-" && !isNegative && !digitRead) {
                 isNegative = true
             } else if (tmp.isNumber() && tmp.toInt() >= 0 && tmp.toInt() <= 9) {
-                integerString.plus(tmp)
+                integerString = integerString.plus(tmp)
                 digitRead = true
             } else if (tmp == "e") {
                 if (digitRead) {
@@ -63,8 +63,34 @@ fun readInteger(input: PushbackInputStream): Int {
 }
 
 
-fun readString(input: PushbackInputStream) {
+fun readString(input: PushbackInputStream): String {
+    val length = readLengthPart(input)
+    var readString = ""
 
+    if (length != 0) {
+        val colon = input.safeRead().toString()
+        if (colon != ":") {
+            throw BencodeReaderException("Excpecting a : in string expression")
+        }
+
+        for (i in 1..4) {
+            readString = readString.plus(input.safeRead().toString())
+        }
+    }
+
+    return String(readString.toByteArray(), Charsets.UTF_8)
+}
+
+private fun readLengthPart(input: PushbackInputStream): Int {
+    val lengthString = input.safeRead().toString()
+    var length: Int? = null
+    if (lengthString.isNumber()) {
+        length = lengthString.toInt()
+    }
+    if (length == null) {
+        throw BencodeReaderException("String length cannot be 0")
+    }
+    return length
 }
 
 fun readDictionary(input: PushbackInputStream) {
